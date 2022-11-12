7184d536f18aaddf21f525915d7507f31a951ffd
Touchsung <jettapat.th@gmail.com>
2022-11-13 00:09:54 +0700

    feat: add navbar component

---
+++ src/components/Navbar.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+const Navbar = () => {
+    return (
+        <nav className="flex justify-between items-center max-w-screen-2xl m-auto px-12 py-12 h-14">
+            <h1 className="text-2xl font-bold text-primaryText tracking-widest hover:cursor-pointer hover:text-primaryBg ease-in-out duration-200">Touch<span className="font-normal">sung</span></h1>
+            <ul className="flex gap-10 text-primaryText text-xl font-semibold font-mono">
+                <li className="p-2">
+                    <a href="/" className=" hover:text-primaryBg ease-in-out duration-200 active:text-primaryText">About</a>
+                </li>
+                <li className="p-2">
+                    <a href="/" className=" hover:text-primaryBg ease-in-out duration-200 active:text-primaryText">Experience</a>
+                </li>
+                <li className="p-2">
+                    <a href="/" className=" hover:text-primaryBg ease-in-out duration-200 active:text-primaryText">Work</a>
+                </li>
+                <li className="p-2">
+                    <a href="/" className=" hover:text-primaryBg ease-in-out duration-200 active:text-primaryText">Contact</a>
+                </li>
+                <li className="p-2">
+                    <a href="/" className=" px-4 py-2 border-2 rounded-lg hover:text-primaryBg ease-in-out duration-200 active:text-primaryText">Resume</a>
+                </li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Navbar
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,13 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
+    colors: {
+      'primaryText': '#002265',
+      'secondaryText': '#14279B',
+      'contentText': '#082032',
+      'primaryBg': '#4581F6',
+      'white': '#FFFFFF',
+    },
     extend: {},
   },
   plugins: [],
