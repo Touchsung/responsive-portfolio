0c750ed4a820e4783b69d84c0c66547e14702fcd
Touchsung <jettapat.th@gmail.com>
2022-11-14 19:40:57 +0700

    fix: hover nav component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -18,20 +18,20 @@ const Navbar = ({ about }) => {
         <nav className="flex justify-between items-center w-full m-auto px-12 py-12 h-14 fixed bg-white100 z-20">
             <h1 className="text-2xl font-bold text-primaryText tracking-widest hover:cursor-pointer hover:text-primaryBg ease-in-out duration-200 z-20">Touch<span className="font-normal">sung</span></h1>
             <ul className={`text-primaryText flex flex-col font-mono font-semibold text-base bg-white100 w-full absolute left-0 items-center z-10 rounded-b-3xl shadow-xl  ${click ? "translate-y-48" : "-translate-y-[500px]"} ease-in-out duration-300 md:flex-row md:translate-y-0 md:relative md:shadow-none md:w-fit md:z-0`}>
-                <li className='py-8 w-full text-center ease-in-out duration-300 md:py-0 md:w-fit md:pl-16 md:hover:text-primaryBg md:relative md:before:content-[""] md:before:bg-primaryBg md:before:w-0 md:hover:before:w-11/12 md:before:ease-out md:before:duration-200 md:before:absolute md:before:h-1  md:before:-bottom-3 md:before:left-9'
+                <li className='py-8 w-full text-center ease-in-out duration-300 md:py-0 md:w-fit md:pl-16 hover:text-primaryBg md:relative md:before:content-[""] md:before:bg-primaryBg md:before:w-0 md:hover:before:w-11/12 md:before:ease-out md:before:duration-300 md:before:absolute md:before:h-[2px]  md:before:-bottom-3 md:before:left-9'
                     onClick={() => {
                         closeMenu()
                         scrollToSection("about")
                     }} >About</li>
-                <li className='py-8 w-full text-center ease-in-out duration-300 md:py-0 md:w-fit md:pl-16 md:hover:text-primaryBg md:relative md:before:content-[""] md:before:bg-primaryBg md:before:w-0 md:hover:before:w-11/12 md:before:ease-out md:before:duration-200 md:before:absolute md:before:h-1  md:before:-bottom-3 md:before:left-9' onClick={() => {
+                <li className='py-8 w-full text-center ease-in-out duration-300 md:py-0 md:w-fit md:pl-16 hover:text-primaryBg md:relative md:before:content-[""] md:before:bg-primaryBg md:before:w-0 md:hover:before:w-11/12 md:before:ease-out md:before:duration-300 md:before:absolute md:before:h-[2px]  md:before:-bottom-3 md:before:left-9' onClick={() => {
                     closeMenu()
                     scrollToSection("experience")
                 }} >Experience</li>
-                <li className='py-8 w-full text-center ease-in-out duration-300 md:py-0 md:w-fit md:pl-16 md:hover:text-primaryBg md:relative md:before:content-[""] md:before:bg-primaryBg md:before:w-0 md:hover:before:w-11/12 md:before:ease-out md:before:duration-200 md:before:absolute md:before:h-1  md:before:-bottom-3 md:before:left-9' onClick={() => {
+                <li className='py-8 w-full text-center ease-in-out duration-300 md:py-0 md:w-fit md:pl-16 hover:text-primaryBg md:relative md:before:content-[""] md:before:bg-primaryBg md:before:w-0 md:hover:before:w-11/12 md:before:ease-out md:before:duration-300 md:before:absolute md:before:h-[2px]  md:before:-bottom-3 md:before:left-9' onClick={() => {
                     closeMenu()
                     scrollToSection("contact")
                 }}>Contact</li>
-                <li className='py-8 w-full text-center ease-in-out duration-300 md:py-0 md:w-fit md:pl-16 md:text-primaryText md:hover:text-primaryBg md:relative md:before:content-[""]  md:before:border-[3px] md:before:border-primaryText md:before:hover:border-primaryBg  md:before:w-full md:before:absolute md:before:h-14  md:before:-top-[16px] md:before:left-8 md:before:-z-10 md:before:rounded-xl md:hover:bg-opacity-0 md:hover:before:bg-opacity-90 md:before:ease-in md:before:duration-300'
+                <li className='py-8 w-full text-center ease-in-out duration-300 md:py-0 md:w-fit md:pl-16 md:text-primaryText hover:text-primaryBg md:relative md:before:content-[""]  md:before:border-[3px] md:before:border-primaryText md:before:hover:border-primaryBg  md:before:w-full md:before:absolute md:before:h-14  md:before:-top-[16px] md:before:left-8 md:before:-z-10 md:before:rounded-xl md:hover:bg-opacity-0 md:hover:before:bg-opacity-90 md:before:ease-in md:before:duration-300 md:mr-8'
                     onClick={() => {
                         closeMenu()
                     }}><a href='https://drive.google.com/file/d/1uu2SoHxqS3bQ3ZtMmn3znjptncuc3t6E/view?usp=sharing'>Resume</a></li>
