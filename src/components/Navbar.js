252897b5d1dc87397521e906012c3cb25dfbd91e
Touchsung <jettapat.th@gmail.com>
2022-11-14 19:41:21 +0700

    fix:delete comments  nav component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -39,38 +39,6 @@ const Navbar = ({ about }) => {
             <div className=' text-primary-text-color text-3xl md:hidden z-20' onClick={handleClick}>
                 {click ? <AiOutlineClose /> : <FiMenu />}
             </div>
-            {/* <ul className={`text-primaryText text-base font-mono font-semibold flex flex-col bg-white100 w-full absolute left-0 items-center z-10 shadow rounded-b-3xl  ${click ? "translate-y-12" : "-translate-y-[550px]"} ease-in-out duration-300 top-7 md:flex-row md:relative md:top-0 md:translate-y-0 md:left-0 md:w-fit gap-6 md:shadow-none`}>
-                <li className="p-2 hover:text-primaryBg ease-in-out duration-200 active:text-primaryText md:py-0 my-4"
-                    onClick={() => {
-                        closeMenu()
-                        scrollToSection("about")
-                    }}
-                >
-                    About
-                </li>
-                <li className="p-2 hover:text-primaryBg ease-in-out duration-200 active:text-primaryText md:py-0 my-4"
-                    onClick={() => {
-                        closeMenu()
-                        scrollToSection("experience")
-                    }}
-                >
-                    Experience
-                </li>
-                <li className="p-2 hover:text-primaryBg ease-in-out duration-200 active:text-primaryText md:py-0 my-4"
-                    onClick={() => {
-                        closeMenu()
-                        scrollToSection("contact")
-                    }}
-                >
-                    Contact
-                </li>
-                <li className="p-2">
-                    <a href="/" className=" px-4 md:py-2 border-2 rounded-lg hover:text-primaryBg ease-in-out duration-200active:text-primaryText sm:border-none my-4">Resume</a>
-                </li>
-            </ul>
-            <div className=' text-primaryText hover:text-primaryBg ease-in-out duration-200 active:text-primaryText text-3xl z-20 md:hidden ' onClick={handleClick}>
-                {click ? <AiOutlineClose /> : <FiMenu />}
-            </div> */}
         </nav>
     )
 }
